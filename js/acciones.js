123c7fd6effe164ee55864bb3be2f09c4bcda418
AL24J111 <osromacu@gmail.com>
2013-03-21 08:16:32 -0600

    1.1.1

--- js/acciones.js
+++ js/acciones.js
@@ -29,6 +29,19 @@ document.addEventListener("deviceready", function(){
 		$('#derecha').swiperight(function(){
 			navigator.notification.alert("deslizó a la derecha",function(){},"practica 1","Aceptar");
 		});
+		$('#izquierda').swipeleft(function){
+			navigator.notification.confirm('¿que desea aser?',function(opcion){
+				switch(opcion){
+					case 1:
+						navigator.notification.beep(1)
+						break;
+						case 2:
+							navigator.notification.vibrate(500)
+							break;
+				}
+		};"practica1","beep,vibrar,cancelar");//function confirm
+		});//function swipe
+		
 	});
 },false);
 });
